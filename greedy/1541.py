27e62fb6364cb6a5d7774267220681eb528c3f5e
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-01-03 00:38:34 +0900

    SOLVE Q_1541

--- greedy/1541.py
+++ greedy/1541.py
@@ -1,41 +1,73 @@
-all = input()
-makeAll = []
-flag = 0 # flag가 1이라는 것은 ( 여기서 마무리했다는 것
+
+all = input().split("-")
+
 for i in range(len(all)):
-    print(all[i])
-    if (all[i] == '-') & (flag == 0): # 마이너스 뒷부분만 젤 크게 만들어주면 되는것?
-        print("11111111")
+    if ('+' in all[i]):
+        all[i] = sum(int(x) for x in all[i].split('+'))
+sum = int(all[0])
+for i in range(1,len(all)):
+    sum = sum - int(all[i])
+print(sum)
+
+"""
+
+#import sys
+#import re
+
+# +,- 기준으로 split하고 delimiter도 없애지 않는다
+#equation = re.split("(\\+|-)", sys.stdin.readline().strip())
+#print(equation)
+
+
+all = all.replace("-0","-")
+all = all.replace("+0","+")
+
+
+
+makeAll = []
+flag = 0
+for i in range(len(all)):
+    if (all[i] == '-') & (flag == 0):
         makeAll.append(all[i])
         makeAll.append('(')
         flag = 1
 
-    elif (all[i] == '-') & (flag == 1): # 마이너스 뒷부분만 젤 크게 만들어주면 되는것? -(    -
-        print("2222222")
+    elif (all[i] == '-') & (flag == 1):
         makeAll.append(')')
         makeAll.append(all[i])
         makeAll.append('(')
 
     elif (all[i] == '+'):
-        print("33333333")
         makeAll.append(all[i])
 
     elif (i == len(all)-1) & (flag == 1):
-        print("444444444")
         makeAll.append(all[i])
         makeAll.append(')')
         flag = 0
-
     else:
-        print("555555555")
         makeAll.append(all[i])
-    print("flag", flag)
-    print("-------------")
 
 
-
+print(makeAll)
 join_All = ""
 for i in range(len(makeAll)):
     join_All += makeAll[i]
+
+print("aaaaaaa")
 print(join_All)
-result = eval(join_All)
-print(result)
+join_All.replace("^0+","")
+print(join_All.
+#result = exec(join_All)
+#print(result)
+
+
+
+"""
+"""
+n = [sum(int(x) for x in y.split('+')) for y in input().split('-')]
+print(n)
+print(sum)
+print(n[0] - sum(n[1:]))
+
+"""
+
