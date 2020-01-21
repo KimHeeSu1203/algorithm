3497c74059396f6731c5d6af5f1dc38adc30e57a
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-01-21 19:35:39 +0900

    SOLVE Q_2178 [백준 미로탐색]

--- bfs/2178.py
+++ bfs/2178.py
@@ -5,47 +5,38 @@ for i in range(N):
     oneline = input()
     maze.append(oneline)
 maze_visited = [[0 for _ in range(M)] for _ in range(N)]
-
-all_result = [] # 이거 어디에 써야하는지 아직 모르겠음
-result = 1
-stack = []
-stack.append([0,0])
+maze_count = [[0 for _ in range(M)] for _ in range(N)]
+queue = []
+queue.append([0, 0])
 maze_visited[0][0] = 1
+maze_count[0][0] = 1
 
-while stack:
-    print("stack : ",stack)
-    print("result : ", result)
-    [i,j] = stack.pop()
+while queue:
+    [i,j] = queue.pop(0)
     if i == (N-1) and j == (M-1):
-        maze_visited[N - 1][M - 1] = 0
-        all_result.append(result)
-    result += 1
+        result = maze_count[N-1][M-1]
+        print(result)
 
     if i > 0:
         if maze[i-1][j] == '1' and maze_visited[i-1][j] == 0:
-            stack.append([i-1,j])
+            queue.append([i-1,j])
             maze_visited[i-1][j] = 1
-            #maze_visited[N-1][M-1] = 0
+            maze_count[i-1][j] = maze_count[i][j] + 1
 
     if i < N-1:
         if maze[i+1][j] == '1' and maze_visited[i+1][j] == 0:
-            stack.append([i+1,j])
+            queue.append([i+1,j])
             maze_visited[i+1][j] = 1
-            #maze_visited[N - 1][M - 1] = 0
+            maze_count[i+1][j] = maze_count[i][j] + 1
 
     if j > 0:
         if maze[i][j-1] == '1' and maze_visited[i][j-1] == 0:
-            stack.append([i,j-1])
+            queue.append([i,j-1])
             maze_visited[i][j-1] = 1
-            #maze_visited[N - 1][M - 1] = 0
+            maze_count[i][j-1] = maze_count[i][j] + 1
 
     if j < M-1:
         if maze[i][j+1] == '1' and maze_visited[i][j+1] == 0:
-            stack.append([i,j+1])
+            queue.append([i,j+1])
             maze_visited[i][j+1] = 1
-            #maze_visited[N - 1][M - 1] = 0
-
-print(all_result)
-all_result.sort()
-answer = all_result[0] - len(all_result)+1
-print(answer)
+            maze_count[i][j+1] = maze_count[i][j] + 1
