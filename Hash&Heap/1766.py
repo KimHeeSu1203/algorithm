65bd829139cd3c3a09fc5c8abb75e195778c40a6
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-02-09 17:56:40 +0900

    UNSOLVE Q_1766 [백준 문제집] ver.2

--- Hash&Heap/1766.py
+++ Hash&Heap/1766.py
@@ -1,16 +1,31 @@
 from collections import defaultdict
-n,m = map(int, input().split())
+import sys
+n,m = map(int, sys.stdin.readline().split())
 
 d = defaultdict(set)
 num_list = [0 for _ in range(n+1)]
 
 for _ in range(m):
-    f_num, n_num = map(int,input().split())
+    f_num, n_num = map(int,sys.stdin.readline().split())
     d[f_num].add(n_num)
     num_list[n_num] += 1
 
 final_list = []
 
+while len(final_list) != n:
+    for i in range(1, n + 1):
+        if (str(i) not in final_list) and (num_list[i] == 0):
+            final_list.extend(str(i))
+
+            for j in range(len(d[i])):
+                list_d = list(d[i])
+                num_list[list_d[j]] -= 1
+                break
+            break
+
+print(" ".join(final_list))
+
+"""
 for i in range(1,n+1):
     if (str(i) not in final_list) and (num_list[i] == 0):
         final_list.extend(str(i))
@@ -23,4 +38,4 @@ for i in range(1,n+1):
                 if (list_d[k] not in final_list) and (num_list[list_d[k]] == 0):
                     final_list.extend(str(list_d[k]))
 
-print(" ".join(final_list))
+"""
