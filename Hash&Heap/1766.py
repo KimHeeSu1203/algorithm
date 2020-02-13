6531debd79752c431954f434ac4e75c59bf575c6
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-02-13 15:37:34 +0900

    UNSOLVE Q_1766 [백준 문제집] ver.3

--- Hash&Heap/1766.py
+++ Hash&Heap/1766.py
@@ -1,41 +1,30 @@
 from collections import defaultdict
 import sys
+
 n,m = map(int, sys.stdin.readline().split())
 
-d = defaultdict(set)
-num_list = [0 for _ in range(n+1)]
+list = [[] for _ in range(n+1)]
+count = [0 for _ in range(n+1)]
 
 for _ in range(m):
-    f_num, n_num = map(int,sys.stdin.readline().split())
-    d[f_num].add(n_num)
-    num_list[n_num] += 1
+    a, b = map(int, sys.stdin.readline().split())
+    list[a].append(b)
+    list[a].sort()
+    count[b] += 1
 
 final_list = []
-
-while len(final_list) != n:
-    for i in range(1, n + 1):
-        if (str(i) not in final_list) and (num_list[i] == 0):
-            final_list.extend(str(i))
-
-            for j in range(len(d[i])):
-                list_d = list(d[i])
-                num_list[list_d[j]] -= 1
-                break
-            break
-
-print(" ".join(final_list))
-
-"""
+real_final_list = []
 for i in range(1,n+1):
-    if (str(i) not in final_list) and (num_list[i] == 0):
-        final_list.extend(str(i))
+    if (count[i] == 0): # 우선순위 문제 없으면
+        final_list.append(i)
+        while (final_list):
+            num = final_list.pop(0)
+            real_final_list.append(num)
+            for j in range(len(list[num])):
+                count[list[num][j]] -= 1  # 1씩 깎아줌
+                if count[list[num][j]] == 0:
+                    final_list.append(list[num][j])
 
-        for j in range(len(d[i])):
-            list_d = list(d[i])
-            num_list[list_d[j]] -= 1
 
-            for k in range(len(list_d)):
-                if (list_d[k] not in final_list) and (num_list[list_d[k]] == 0):
-                    final_list.extend(str(list_d[k]))
-
-"""
+for elem in real_final_list:
+    print(elem, end = " ")
