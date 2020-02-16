b8394aa81efb6b9ba7a20324b622df9e68ddc65f
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-02-16 23:24:06 +0900

    UNSOLVE Q_1766 [프로그래머스 큰 수 만들기] ver.3

--- greedy/42883.py
+++ greedy/42883.py
@@ -3,15 +3,17 @@ def solution(number, k):
     number = list(str(number))
     stack = []
     stack.append(number[0])
+    m = 0
     for i in range(1,len(number)):
         stack.append(number[i])
-        print("stack", stack)
         size = len(stack)-1
-        if(stack[size-1]<stack[size]) & (k>=0):
-            stack.pop(i-1)
-            k -= 1
-            print("k",k)
-            print("stack", stack)
+        print("m1",m)
+        print("stack", stack)
+        if(stack[size-1]<stack[size-2]) & (m!=k):
+            stack.pop(size-1)
+            print(stack)
+            m += 1
+            print("m2", m)
 
     answer = "".join(stack)[:-k]
     return answer
