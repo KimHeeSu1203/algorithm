c76b05be43c0e00d0b5fd8b429a039dcc17704df
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-02-18 23:09:24 +0900

    SOLVE Q_42885 [프로그래머스 구명보트]

--- greedy/42885.py
+++ greedy/42885.py
@@ -1,23 +1,24 @@
 def solution(people, limit):
     answer = 0
-    visit = [0 for _ in range(len(people))]
-    print(visit)
     people.sort()
-    for i in range(len(people)):
-        if(visit[i] == 0):
-            min = people[i]
-            visit[i] = 1
-            for j in range(len(people)-1,i,-1):
-                if(min+people[j] <= limit) and (visit[j] ==0): #합이 낮고, 들린 적 없으면
-                    visit[j] = 1
+
+    min = 0
+    max = len(people)-1
+
+    while(min <= max):
+        #print("max, min, answer", max,people[max], min,people[min], answer)
+        if(people[min] + people[max] <= limit):
             answer += 1
+            min += 1
+            max -= 1
+        else:
+            answer += 1
+            max -= 1
+
     return answer
 
 
-people = [20, 70, 80, 50]
+people = [80, 80, 80, 80, 20]
 limit = 100
 
 print(solution(people,limit))
-
-
-##배 2척 말고 3척, 4척이 넘게도 합쳐 질 수 있다는 점을
