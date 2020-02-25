47ec6e7cf989c8816cf67ad96a97fb8f41888ef6
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-02-25 16:45:24 +0900

    UNSOLVE Q_42747 [프로그래머스 h-index] ver.2

--- sort/42747.py
+++ sort/42747.py
@@ -2,13 +2,15 @@ def solution(citations):
     length = len(citations)
     blink = [0 for _ in range(length)]
 
-    for i in citations:
+    for i in range(len(citations),-1,-1):
         answer = 0
-        for j in citations:
-            if (i<=j):
+        for tmp in citations:
+            if tmp >= i:
                 answer += 1
-        if answer == i:
+        if answer == i :
             return answer
+
+    return answer
 
 citations = [3, 0, 6, 1, 5]
 print(solution(citations))
