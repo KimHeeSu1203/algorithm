80b778f0a7c46c3aec2c33c8cb511f17007e7a21
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-03-10 00:59:30 +0900

    UNSOLVE Q_42583 [프로그래머스 다리를 지나는 트럭 시간초과 ver2 (정확도 92.3)]

--- Queue&Stack/42583.py
+++ Queue&Stack/42583.py
@@ -1,15 +1,19 @@
+from collections import deque
+
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    bridge = [0 for _ in range(bridge_length)]
+    bridge = deque(0 for _ in range(bridge_length))
+    truck_weights = deque(truck_weights)
+    
     while(truck_weights):
         answer += 1
-        if((sum(bridge[1:])+truck_weights[0]) > weight): # 무게 더나가면
+        if((sum(list(bridge)[1:])+truck_weights[0]) > weight): # 무게 더나가면
             bridge.append(0)
-            bridge.pop(0)
+            bridge.popleft()
         else:
             bridge.append(truck_weights[0])
-            truck_weights.pop(0)
-            bridge.pop(0)
+            truck_weights.popleft()
+            bridge.popleft()
     answer += bridge_length
     return answer
 
