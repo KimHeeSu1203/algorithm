17019a6661a18bfeb199a62932afc180d3ab7ff2
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-03-11 15:42:56 +0900

    SOLVE Q_42584 [백준 주식가격]

---
+++ Queue&Stack/42584.py
@@ -0,0 +1,15 @@
+def solution(prices):
+    answer = []
+    for i in range(len(prices)):
+        stack = []
+        for j in range(i+1,len(prices)):
+            if prices[j]>=prices[i]:
+                stack.append(j)
+            else:
+                stack.append(j)
+                break
+        answer.append(len(stack))
+    return answer
+
+prices = [1, 2, 3, 2, 3]
+print(solution(prices))
