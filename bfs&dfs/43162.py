fec8916edc746ad4cfe1ee422b914db795a31437
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-03-20 01:13:23 +0900

    SOLVED Q_43162 [프로그래머스 네트워크]

--- bfs&dfs/43162.py
+++ bfs&dfs/43162.py
@@ -1,17 +1,26 @@
+def dfs(n,computers,visit,start):
+    tmp = [start] # 친구인 애들 담는
+    while(tmp):
+        x = tmp.pop(0)
+        if visit[x]==0:
+            visit[x]=1
+        for i in range(n):
+            if(computers[x][i]==1) and (visit[i]==0):
+                tmp.append(i)
+
+
 def solution(n, computers):
     answer = 0
-    all = []
-    for i in range(n):
-        tmp = []
-        for j in range(n):
-            if computers[i][j] == 1:
-                tmp.append(j)
-        all.append(tmp)
-    print(all)
-    all = list(set(map(tuple,all)))
-    return len(all)
+    visit = [0]*n
+    start = 0
+    while 0 in visit:
+        if visit[start] == 0:
+            dfs(n,computers,visit,start)
+            answer +=1
+        start+=1
+    return answer
 
 
 n = 3
-computers = [[1, 1, 0], [1, 1, 1], [0, 1, 1]]
+computers = [[1, 1, 0], [1, 1, 1], [0, 0, 1]]
 print(solution(n,computers))
