d4b67e0869ad85db9146005c28cd6bf437de0664
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-09-03 14:08:04 +0900

    UNSOLVE Q_42889 [프로그래머스 실패율]

---
+++ programmers/42889.py
@@ -0,0 +1,18 @@
+# runtime error
+def solution(N, stages):
+    dict_stage = {}
+    tmp_answer = [0 for _ in range(N+1)]
+    answer = [0 for _ in range(N)]
+    for stage in stages:
+        for i in range(stage):
+            tmp_answer[i] = tmp_answer[i] + 1
+
+    for i in range(N):
+        answer[i] = (tmp_answer[i]-tmp_answer[i+1]) / tmp_answer[i]
+
+    answer = [i[0]+1 for i in sorted(enumerate(answer), key=lambda x: [-x[1],x[0]])]
+    return answer
+
+N = 5
+stages = [2, 1, 2, 6, 2, 4, 3, 3]
+print(solution(N,stages))
